6529d3aaa25393b9e6d53b6cee1b8729c7d2684b
fmarcos38 <fmarcos_23@hotmail.com>
2025-06-20 15:40:25 -0300

    mp

--- src/routes/mercadopago.js
+++ src/routes/mercadopago.js
@@ -4,22 +4,8 @@ const Carrito = require('../models/carrito');
 const Producto = require('../models/producto');
 
 const router = express.Router();
-const ACCESS_TOKEN = '5099c2f8082c889782a8e14d4c292e8d1b3543dd7134c7b543bc50cfecd5b4ab';
+//const ACCESS_TOKEN = '5099c2f8082c889782a8e14d4c292e8d1b3543dd7134c7b543bc50cfecd5b4ab';
 
-//Revisá si querés quedarte con paymentMP (con axios) o crearPreferencia (con el SDK). 
-//router.post('/crear-preferencia', crearPreferencia); 
-router.post('/crear-preferencia', paymentMP); //mismo que la de arriba
-
-
-router.post('/notificaciones', recibirNotificaciones);
-
-
-
-//ruta de prueba del webhook MP
-/* router.post('/webhooks/mercadopago', (req, res) => {
-    console.log('Notificación recibida:', req.body);
-    res.sendStatus(200); // Importante responder 200 OK
-}); */
 router.post('/webhooks/mercadopago', async (req, res) => {
     try {
         const payment = req.body;
@@ -62,6 +48,67 @@ router.post('/webhooks/mercadopago', async (req, res) => {
         res.sendStatus(500);
     }
 });
+
+//router.post('/crear-preferencia', paymentMP);
+router.post('/', async(req, res) => {
+    const { body } = req.body;  //console.log("body: ", body);
+        const usuarioId = body.external_reference; // Debe venir del frontend
+    
+        const payload = {
+            ...body,
+            external_reference: usuarioId,
+            back_urls: {
+                success: `${process.env.URL}/success`,
+                failure: `${process.env.URL}/failure`,
+                pending: `${process.env.URL}/pending`,
+            },
+            auto_return: 'approved',
+            //ver que url corresponde
+            notification_url: `http://backendpaletas-0e5d1e2325c9.herokuapp.com/mercadopago/webhooks/mercadopago`,
+        };
+    
+        const url = "https://api.mercadopago.com/checkout/preferences";
+    
+        try {
+            const payment = await axios.post(url, payload, {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${process.env.MERCADOPAGO_ACCESS_TOKEN}`,
+                },
+            });
+    
+            const prefId = extractPrefId(payment.data.init_point);
+            res.send({ url: prefId });
+    
+        } catch (error) {
+            console.error("Error al crear preferencia:", error);
+            res.status(500).json({ message: 'Error al crear preferencia' });
+        }
+});
+
+//router.post('/notificaciones', recibirNotificaciones);
+router.post('notificaciones', async(req, res) =>{
+    try {
+            const { id, topic } = req.query;
+    
+            if (topic === 'payment') {
+                const payment = await mercadopago.payment.findById(id);
+                console.log('Notificación de pago recibida:', payment);
+                // Aquí puedes manejar la lógica de actualización de estado de pago en tu base de datos
+            }
+    
+            res.sendStatus(200);
+        } catch (error) {
+            console.error('Error al recibir notificación de pago:', error);
+            res.status(500).json({ message: 'Error al recibir notificación de pago' });
+        }
+});
+
+//ruta de prueba del webhook MP
+/* router.post('/webhooks/mercadopago', (req, res) => {
+    console.log('Notificación recibida:', req.body);
+    res.sendStatus(200); // Importante responder 200 OK
+}); */
 
 
 router.get('/verificar-pago/:id', async (req, res) => {
